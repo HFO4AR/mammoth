91ae0a5c9f8efb996c2d463b30238931c25d3e56
HFO4AR <amikono728@gmail.com>
2025-11-21 21:59:15 +0800

    feat(main): 添加LED闪烁功能并配置调试环境
    - 在main.cpp中引入GPIO驱动并实现LED蓝灯闪烁逻辑
    - 配置J-Link调试脚本支持WSL路径映射
    - 更新IDEA项目配置以适配新的工具链和板级设置
    - 添加对CAN接口及追踪功能的内核配置支持

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,27 @@
 #include "main.h"
 #include "zephyr/kernel.h"
 #include "can.h"
+#include <zephyr/drivers/gpio.h>
+static const struct gpio_dt_spec led_blue = GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
 int Init() {
     CanInit(chassis_can_dev);
     return 1;
 }
-
+int a=0;
 int main() {
     if (Init()) {
         printk("初始化成功");
-    }else {
+    } else {
         printk("初始化失败");
         return 0;
+    }
+
+    gpio_pin_configure_dt(&led_blue, GPIO_OUTPUT_LOW);
+    while (1) {
+        // 切换LED状态
+        gpio_pin_toggle_dt(&led_blue);
+        // 延时200毫秒，实现LED闪烁效果
+        k_msleep(200);
+        a++;
     }
 }
